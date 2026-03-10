13f21aa80f827c5041378bed2b8a350dd5f28b23
Sushmasree Rudroju <sushmasree.rudroju@gmail.com>
2026-03-09 20:36:25 -0500

    Enhance smoothie app with fruit search functionality
    Updated the smoothie app to include a search feature for fruit ingredients. Added SEARCH_ON column for API requests and improved ingredient selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -14,8 +15,9 @@ name_on_order = st.text_input('Name on Smoothie')
 st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.write(my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections =5)
 
 if ingredients_list:
@@ -24,8 +26,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
